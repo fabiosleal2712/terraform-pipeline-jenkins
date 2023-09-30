b434fc99c47db25195ca674bdd6d63ed8fa46613
fabiosleal2712 <lymaleal@gmail.com>
2023-09-30 19:35:01 -0300

    add porta 8080, 80 e 8084

--- sg.tf
+++ sg.tf
@@ -41,5 +41,33 @@ resource "aws_security_group" "ssh" {
     protocol    = "-1"  # Todas as portas e protocolos
     cidr_blocks = ["0.0.0.0/0"]  # Permite acesso a qualquer destino (não recomendado para produção)
   }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Permite acesso de qualquer IP (não recomendado para produção)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Todas as portas e protocolos
+    cidr_blocks = ["0.0.0.0/0"]  # Permite acesso a qualquer destino (não recomendado para produção)
+  }
+
+  ingress {
+    from_port   = 8084
+    to_port     = 8084
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Permite acesso de qualquer IP (não recomendado para produção)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Todas as portas e protocolos
+    cidr_blocks = ["0.0.0.0/0"]  # Permite acesso a qualquer destino (não recomendado para produção)
+  }
 }
 
