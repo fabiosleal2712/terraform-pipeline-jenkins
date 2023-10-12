9350a1f8ccae82bf7ff218d5e39b0cd0f3be5d47
fabiosleal2712 <lymaleal@gmail.com>
2023-10-12 13:12:39 -0300

    instalando zeroteir

--- vm.tf
+++ vm.tf
@@ -14,19 +14,19 @@ resource "aws_instance" "ec2_instance" {
     Name = "EC2-Instance"
   }
 
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              yum install -y docker
-              service docker start
-              usermod -a -G docker ec2-user
-              chkconfig docker on
-              curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-              chmod +x /usr/local/bin/docker-compose
-              mkdir -p /home/ec2-user/nginx/_data
-              mkdir -p /home/ec2-user/nginx/dornetdocker
-              aws s3 sync s3://terraformstate847974/_data /home/ec2-user/nginx/_data
-              aws s3 sync s3://terraformstate847974/dornetdocker /home/ec2-user/nginx/dornetdocker
-              chown -R ec2-user:ec2-user /home/ec2-user/nginx
-              EOF
+#  user_data = <<-EOF
+#              #!/bin/bash
+#              yum update -y
+#              yum install -y docker
+#              service docker start
+#              usermod -a -G docker ec2-user
+#              chkconfig docker on
+#              curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+#              chmod +x /usr/local/bin/docker-compose
+#              mkdir -p /home/ec2-user/nginx/_data
+#              mkdir -p /home/ec2-user/nginx/dornetdocker
+#              aws s3 sync s3://terraformstate847974/_data /home/ec2-user/nginx/_data
+#              aws s3 sync s3://terraformstate847974/dornetdocker /home/ec2-user/nginx/dornetdocker
+#              chown -R ec2-user:ec2-user /home/ec2-user/nginx
+#              EOF
 }
