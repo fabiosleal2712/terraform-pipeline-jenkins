d75f242dc5df9a187b50e8e0758443961c0df106
fabiosleal2712 <lymaleal@gmail.com>
2023-12-03 15:59:00 -0300

    ADD port 443 on sg

--- sg.tf
+++ sg.tf
@@ -48,7 +48,13 @@ resource "aws_security_group" "ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]  # Permite acesso de qualquer IP (não recomendado para produção)
   }
-
+  
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Permite acesso de qualquer IP (não recomendado para produção)
+  }
   egress {
     from_port   = 0
     to_port     = 0
