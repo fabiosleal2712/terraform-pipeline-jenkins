a317c266c157f4aeec0d7316460a8f28c2459bad
fabiosleal2712 <lymaleal@gmail.com>
2023-12-03 20:57:54 -0300

    destroy

--- vm.tf
+++ vm.tf
@@ -16,13 +16,20 @@ resource "aws_instance" "ec2_instance" {
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC19wrMUDmfm+WgT0ZpK49udaOc7W/finJeSFJCIlfASYbX60lg2i/jkzWrX+bnCFla8V8QkzRaL0Ja2kO27siN5yAyoyp8yJ9CiZkaVPGk4lWTw5G71MtSm3uKi9BsdZo2lk2HWvevb4NWNopJum3JP08TZ6rRh/UZozIfyZvS8XZaXo6e2HB2JDJrpcUFaClv3PpdcfbIK/Vx1rOfLdeG3PZrbA29yk76mj5VxI2W3dW64U2h2m6lp0IHv1eNlxItQ+1fZsoJnXr/62PfA/rJ4jAS6/Sx1S4P5v3NiE1FB6cSnrDRywUYxj1h/WMLt1PBvHugy1Mj7/sXzVwgyL5NRDefsosmHQzPyQkitD024pFF/GUVkzznNolkp/0RG6aKmbx26O+PKLIY0AsJUIDrn1W57OKL6i4C/bFl0PDWaND87i7A1cucdYqyPOYHBSTKXwi3ccTT6NGauDDZ2E8iEy9i0RVPRWJEtX4/hh9QC3+04cvazq/uFX5JXHn4fZE= jenkins@6675950ac259" >> /home/ec2-user/.ssh/authorized_keys
-              sudo apt update
-              sudo apt -y install docker
-              sudo service docker start
-              sudo usermod -a -G docker ubuntu
-              sudo chkconfig docker on
-              sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-              sudo chmod +x /usr/local/bin/docker-compose
+              # Add Docker's official GPG key:
+              sudo apt-get update
+              sudo apt-get install ca-certificates curl gnupg -y
+              sudo install -m 0755 -d /etc/apt/keyrings
+              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+              sudo chmod a+r /etc/apt/keyrings/docker.gpg
+              sudo usermod -aG docker ubuntu
+              # Add the repository to Apt sources:
+              echo \
+                "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+                $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+                sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+              sudo apt-get update
+              sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+              sudo apt install docker-compose -y
               EOF
 }
