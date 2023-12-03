f24d1390be2101038676fa1949df8f95edff3a58
fabiosleal2712 <lymaleal@gmail.com>
2023-12-03 18:52:17 -0300

    add porta entrada

--- sg.tf
+++ sg.tf
@@ -49,6 +49,12 @@ resource "aws_security_group" "ssh" {
     cidr_blocks = ["0.0.0.0/0"]  # Permite acesso de qualquer IP (não recomendado para produção)
   }
   
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Todas as portas e protocolos
+    cidr_blocks = ["0.0.0.0/0"]  # Permite acesso a qualquer destino (não recomendado para produção)
+
   ingress {
     from_port   = 443
     to_port     = 443
