a3625f9dc206e4d268bf220c1c1982f233460212
fabiosleal2712 <lymaleal@gmail.com>
2023-12-03 21:01:25 -0300

    destroy

--- vm.tf
+++ vm.tf
@@ -23,7 +23,6 @@ resource "aws_instance" "ec2_instance" {
               curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
               sudo chmod a+r /etc/apt/keyrings/docker.gpg
               sudo usermod -aG docker ubuntu
-              # Add the repository to Apt sources:
               echo \
                 "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
                 $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
